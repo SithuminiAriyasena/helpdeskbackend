0c401bb427c8e8de980d403bd9a8aa2a37f1ec4d
SithuminiAriyasena <sithuminiariyasena@gmail.com>
2026-09-06 19:41:12 +0530

    feat(auth): return avatar and department on login

--- controllers/authController.js
+++ controllers/authController.js
@@ -92,7 +92,9 @@ exports.login = async (req, res) => {
         id: user.id,
         name: user.name,
         email: user.email,
-        role: user.role
+        role: user.role,
+        avatar: user.avatar || null,
+        department: user.department || null,
       }
     });
   } catch (error) {
